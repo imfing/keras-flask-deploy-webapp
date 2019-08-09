b1c234afecc875bcfdcf337474d8c678edb8d19e
Sergej Grilborzer <sergej1995@gmail.com>
2019-08-09 10:20:23 +0200

    Add host configuration to WSGIServer
    Adding host parameter '0.0.0.0' enables compatibility on Firefox

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ if __name__ == '__main__':
     # app.run(port=5002, debug=True)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('', 5000), app)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
