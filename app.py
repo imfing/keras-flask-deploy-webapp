0ef079eaa1b5664ee208e69eaf478f9a7c03c86e
Hameem Dakheel <HameemDakheel@outlook.com>
2023-03-30 21:05:27 +0200

    dev

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'keras_model.h5'
+MODEL_PATH = 'model.h5'
 
 # Load your own trained model
 # model = load_model(MODEL_PATH)
@@ -49,10 +49,12 @@ np.set_printoptions(suppress=True)
 print("Loading model")
 
 # Load the model
-model = load_model(MODEL_PATH, compile=False)
+model = load_model(MODEL_PATH)
 
 # Load the labels
-class_names = open("labels.txt", "r").readlines()
+# class_names = open("labels.txt", "r").readlines()
+class_names = ['pituitary','notumor', 'meningioma', 'glioma']
+
 
 print('Model loaded. Check http://127.0.0.1:5000/')
 
@@ -74,23 +76,31 @@ def model_predict(img, model):
     # Create the array of the right shape to feed into the keras model
     # The 'length' or number of images you can put into the array is
     # determined by the first position in the shape tuple, in this case 1
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    # data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
     # Replace this with the path to your image
-    image = img.convert("RGB")
+    # image = img.convert("RGB")
 
     # resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    # size = (224, 224)
+    size = (128, 128)
+    # image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    image = ImageOps.fit(img, size, Image.Resampling.LANCZOS)
 
     # turn the image into a numpy array
     image_array = np.asarray(image)
 
     # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    # normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    normalized_image_array = image_array/255
+
 
     # Load the image into the array
-    data[0] = normalized_image_array
+    # data = normalized_image_array
+
+    # add a batch dimension to the image
+    data = np.expand_dims(normalized_image_array, axis=0)
 
     # Predicts the model
     prediction = model.predict(data)
@@ -125,7 +135,7 @@ def predict():
         class_name = class_names[index]
         confidence_score = str(prediction[0][index])
         result = str(class_name[2:])
-        print("Class:", result, end="")
+        print("Class:", result)
         print("Confidence Score:", confidence_score)
         
         # Serialize the result, you can add additional fields
